6cb160f4e569f0df99d8fb8d6eae1c847fad7b9f
Evrine <minayoevrine@gmail.com>
2023-05-21 23:49:31 +0300

    springboot components

--- src/main/java/com/springboot/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/springboot/springbootdemo/SpringbootDemoApplication.java
@@ -1,14 +1,20 @@
 package com.springboot.springbootdemo;
 
+import com.springboot.springbootdemo.sub1.SpringComponent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
-		System.out.println("Hi Evrine");
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringbootDemoApplication.class, args);
+		SpringComponent component = applicationContext.getBean(SpringComponent.class);
+		System.out.println(component.getMessage());
+
+
+
 	}
 
 }
--- src/main/java/com/springboot/springbootdemo/sub1/SpringComponent.java
+++ src/main/java/com/springboot/springbootdemo/sub1/SpringComponent.java
@@ -1,4 +1,10 @@
 package com.springboot.springbootdemo.sub1;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SpringComponent {
+    public String getMessage() {
+        return "Hello from SpringComponent";
+    }
 }
